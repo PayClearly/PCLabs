df8d4e8a146354b30e0c367de9972c339d2d6d16
Aaron Colby <acolby@live.com>
2020-05-05 14:35:52 -0700

    updated gru to handle deploy

--- packages/gru/index.js
+++ packages/gru/index.js
@@ -6,6 +6,7 @@ const _generateBuild = require('./lib/_generateBuild');
 const _archiveBuild = require('./lib/_archiveBuild');
 const _uploadToStorage = require('./lib/_uploadToStorage');
 const _tagCommit = require('./lib/_tagCommit');
+const _deploy = require('./lib/_deploy');
 
 module.exports = () => {
   
@@ -17,7 +18,7 @@ module.exports = () => {
       console.log('\x1b[35m%s\x1b[0m', `\nStep 1: Inquire for needed information`);
       const { app, cache, deployTo } = await _determineParametersToUse(givenAnswers, config);
 
-      // // step 2: Check git for current tags to make sure a build doesn't already exist for this
+      // step 2: Check git for current tags to make sure a build doesn't already exist for this
       console.log('\x1b[35m%s\x1b[0m', `\nStep 2: Stashing unsaved changes and checking for existing build`);
       const currentBuild = await _checkForCurrentBuild(config, app).then(data => data || {});
       const buildKey = currentBuild.buildKey || _generateBuildKey(config);
@@ -26,11 +27,10 @@ module.exports = () => {
       console.log('\x1b[35m%s\x1b[0m', `\nStep 3: Generate build for deploy`);
       await _generateBuild(config, currentBuild, app, buildKey);
 
-      // // step 4: archive -- will skip if there is a current build
+      // step 4: archive -- will skip if there is a current build
       console.log('\x1b[35m%s\x1b[0m', `\nStep 4: Archive build folder`);
       await _archiveBuild(config, currentBuild, app, buildKey);
 
-      // return early if the desire is to not upload the code
       if (!cache) {
         console.log('\x1b[35m%s\x1b[0m', `\nDONE: the code was only build locally ${{ buildKey }}\n `);
         return { buildKey, tag: undefined };
@@ -40,7 +40,7 @@ module.exports = () => {
       console.log('\x1b[35m%s\x1b[0m', `\nStep 5: Cache build`);
       await _uploadToStorage(config, currentBuild, app, buildKey);
 
-      // // step 6: tag commit with new build -- will skip if there is a current build
+      // step 6: tag commit with new build -- will skip if there is a current build
       console.log('\x1b[35m%s\x1b[0m', `\nStep 6: Tag commit with build key`);
       const tag = await _tagCommit(config, currentBuild, app, buildKey);
 
@@ -49,9 +49,9 @@ module.exports = () => {
         return { buildKey, tag };
       }
 
-      // // step 7: TODO deploy code to desired environment
-      console.log('\x1b[35m%s\x1b[0m', `\nStep 7: Tag commit with build key`);
+      // step 7: deploy code to desired environment
       console.log('\x1b[35m%s\x1b[0m', `\nDONE: the code has been build and deployed ${{ buildKey, tag, env: deployTo }}\n `);
+      await _deploy(config, currentBuild, app, buildKey, deployTo);
       return { buildKey, tag };
     },
   };
---
+++ packages/gru/lib/_deploy.js
@@ -0,0 +1,33 @@
+const shellpromise = require('shellpromise');
+
+const _deploy = async(config, currentBuild, appName, buildKey, env) => {
+
+  await shellpromise('git fetch --tags');
+
+  const envTagPrefix = await config.apps[appName].envTagPrefix;
+  console.log(`Checking for existing tag with prefix '${envTagPrefix}' tag for this app`);
+  const matchedTag = await shellpromise(`git tag --contains HEAD`).catch(data => false).then(data => data.split('\n').find(tag => tag.includes(`${envTagPrefix}_`)));
+
+  if (matchedTag) {
+    console.log(`This build has already been deployed to ${env}`);
+    return {
+      tag: matchedTag,
+      buildKey,
+    };
+  }
+
+  console.log('running the deploy method');
+  await config.apps[appName].deploy(buildKey, env);
+
+  // delete the remote tat
+  console.log('Tagging commit and pushing tag to Git');
+  console.log('...');
+  const tagName = `${envTagPrefix}_${buildKey}`;
+  await shellpromise(`git tag ${tagName}`);
+  await shellpromise(`git push origin :${tagName}`);
+  await shellpromise(`git push origin ${tagName}`);
+  console.log('Successfully tagged and pushed!');
+
+};
+
+module.exports = _deploy;
