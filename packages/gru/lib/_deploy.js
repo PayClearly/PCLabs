462a73cbaf09d5dbb614c973b838dc639108d53a
Aaron Colby <acolby@live.com>
2020-05-10 12:04:04 -0700

    added proper tag check

--- packages/gru/lib/_deploy.js
+++ packages/gru/lib/_deploy.js
@@ -2,8 +2,8 @@
 const _deploy = async (delegates, config, currentBuild, appName, buildKey, env) => {
 
   const envTagPrefix = await config.apps[appName].envTagPrefix;
-  console.log(`Checking for existing tag with prefix '${envTagPrefix}' tag for this app`);
-  const matchedTag = (await delegates.gitFetchTags()).find(tag => tag.includes(`${envTagPrefix}_`));
+  console.log(`Checking for existing tag with prefix '${envTagPrefix}_${env}' tag for this app`);
+  const matchedTag = (await delegates.gitFetchTags()).find(tag => tag.includes(`${envTagPrefix}_${env}`));
 
   if (matchedTag) {
     console.log(`This build has already been deployed to ${env}`);
