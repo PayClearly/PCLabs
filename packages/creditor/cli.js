2d553ffaad1abd42ae182446bbc8954632f32215
Aaron Colby <acolby@live.com>
2020-05-28 15:25:05 -0700

    added added perform to cli prompt

--- packages/creditor/cli.js
+++ packages/creditor/cli.js
@@ -4,7 +4,10 @@ const argv = require('yargs').argv;
 const creditor = _tryRequire('@pclabs/creditor') || _tryRequire('./');
 
 const instance = creditor();
-return instance.prompt(argv);
+return instance.prompt(argv)
+  .then((answers) => {
+    return instance.perform(answers);
+  });
 
 function _tryRequire(path) {
   try {
