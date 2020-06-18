052fcea661208c5f3b08cbe9a766b7caca074163
Aaron Colby <acolby@live.com>
2020-06-17 20:48:38 -0700

    importer backwards compatable

--- packages/creditor/import.js
+++ packages/creditor/import.js
@@ -5,16 +5,16 @@ function dirImport(context) {
   return _nestedExporter(context, context.keys());
 
   // helper nested exporter
-  function _nestedExporter(context, contextItems = []) {
+  function _nestedExporter(context, contextItems) {
     
     const structure = {};
-    (contextItems)
-      .forEach((key) => {
+    (contextItems || [])
+      .forEach(function(key) {
         const keysSplit = key.split('/');
         const name = keysSplit[keysSplit.length - 2];
 
         if (!name || name === '.') return; // return if does not match structure
-        const defaultMod = keysSplit.filter((key) => {
+        const defaultMod = keysSplit.filter(function(key) {
           return key.indexOf('.') < 0;
         });
 
@@ -24,23 +24,23 @@ function dirImport(context) {
         toPush.__creditor = { item: true };
         defaultMod.push(toPush);
 
-        _patchDeep(structure, ...defaultMod);
+        _patchDeep(structure, defaultMod);
       });
     return structure;
   }
 
-  function _patchDeep(toSet, ...rest) {
+  function _patchDeep(toSet, rest) {
     if (rest.length <= 2) {
       const current = toSet[rest[0]];
       if (current) {
         toSet[rest[0]] = rest[1];
-        Object.keys(current).forEach(key => toSet[rest[0]][key] = current[key]);
+        Object.keys(current).forEach(function(key) { return toSet[rest[0]][key] = current[key]; });
       } else {
         toSet[rest[0]] = rest[1];
       }
     } else {
       toSet[rest[0]] = toSet[rest[0]] || { };
-      _patchDeep(toSet[rest[0]], ...rest.slice(1));
+      _patchDeep(toSet[rest[0]], rest.slice(1));
     }
   }
 
